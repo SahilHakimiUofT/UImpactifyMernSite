1cc36dd782d4e78f19570e992b477d4390c0a424
MamrK29 <mohammedamrkhan@gmail.com>
2020-10-20 15:44:08 -0400

    Mak/tbd 39/implement a logout button (#9)
    * [TBD-29] MAK Inital commit of authentication
    
    * [TBD-29] added privateroute classes to prevent users not logged in from accessing the page
    
    * [TBD-29] Completed firebase connectivity, users are able to sign up and login
    
    * [TBD-29] add additional dependcies to package.json
    
    * [TBD-39] completed the logout button with functionality
    
    * TBD-39 edited formatting in logout_button_component.js

--- u-impactify/src/components/logout_button.component.js
+++ u-impactify/src/components/logout_button.component.js
@@ -4,7 +4,7 @@ import app from '../config/firebase';
 export default class LogOutButton extends Component{
     render(){
         return(
-            <div className="lbutton">
+            <div className="logout_button">
                 <button onClick={() => app.auth().signOut()}>Sign out</button>
             </div>
         )
