a4659a71fda7c93bf49169dcb54052fec9f500ad
sofiarahul <sofiarahul7@gmail.com>
2020-10-22 03:07:29 -0400

    social initiative dashboard files plus modified app.js to include organization

---
+++ u-impactify/src/components/socialInitDashboard.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Navbar from './socialInitNavbar.component.js';
+import Footer from './footer.component';
+import './socialInitDashboard.css';
+export default class SocialInitiativeDashboard extends Component{
+    render(){
+    return (
+        <div className = 'lmain'>
+            <Navbar />
+            <div className = 'footer'>
+                 <Footer />
+            </div>
+        </div>
+    );
+
+    }}
