0f11b9a202e07318df05d8182412099cee89d522
Sahil <sahil_hakimi@hotmail.com>
2020-10-21 22:41:42 -0400

    TBD-42 Seperated topbar from sidebar to streamline side navabar for other pages

--- u-impactify/src/components/consultant.js
+++ u-impactify/src/components/consultant.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import Navbar from './navbar.component.js';
 import Footer from './footer.component';
 import './consultant.css';
+import * as CgIcons from 'react-icons/cg';
+import { Link } from 'react-router-dom';
 export default class Consultant extends Component{
     render(){
     return (
         <div className = 'main'>
               <div className = 'content-wrap'>
+              <div className='top-navbar'>
+                <h1 className = "dashboard">Dashboard</h1>
+                
+                <CgIcons.CgProfile size={50} className = 'profile-pic'/>
+               <Link to='./profile' className="profile-link"><h3 >Profile</h3></Link>
+                </div>
+
                 <Navbar />
                 <div className = 'Foot'>
                 <Footer />
