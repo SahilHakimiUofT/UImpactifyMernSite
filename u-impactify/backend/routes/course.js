33e365cb65f8f4f19fc5cedee46a413536e9d3db
wonderge@yahoo.ca <wonderge@yahoo.ca>
2020-11-02 21:01:54 -0500

    [TBD-49] Implemented Add and Update requests

--- u-impactify/backend/routes/course.js
+++ u-impactify/backend/routes/course.js
@@ -14,11 +14,49 @@ router.route('/:id').get((req, res) => {
 });
 
 router.route('/add').post((req, res) => {
+  const _id = req.body._id
+  const name = req.body.name
+  const description = req.body.description
+  const outline = req.body.outline
+  const instructor = req.body.instructor
+  const lessons = req.body.lessons
+  const tasks = req.body.tasks
+  const lessonLength = req.body.lessonLength
+  const startDate = Date.parse(req.body.startDate)
+  const endDate = Date.parse(req.body.endDate)
+  const preReq = req.body.preReq
+  const preReqFor = req.body.preReqFor
+  const difficultyLevel = req.body.difficultyLevel
+  const pictureUrl = req.body.pictureUrl
 
+  const newCourse = new Course({_id, name, description, outline, instructor, lessons, tasks, lessonLength, 
+    startDate, endDate, preReq, preReqFor, difficultyLevel, pictureUrl})
+  
+  newCourse.save()
+    .then(() => res.json('Course Added'))
+    .catch(err => res.status(400).json('Error: ' + err))
 });
 
 router.route('/update/:id').patch((req, res) => {
+  Course.findById(req.params.id)
+    .then(course => {
+      course.name = req.body.name
+      course.description = req.body.description
+      course.outline = req.body.outline
+      course.lessons = req.body.lessons
+      course.tasks = req.body.tasks
+      course.lessonLength = req.body.lessonLength
+      course.startDate = Date.parse(req.body.startDate)
+      course.endDate = Date.parse(req.body.endDate)
+      course.preReq = req.body.preReq
+      course.preReqFor = req.body.preReqFor
+      course.difficultyLevel = req.body.difficultyLevel
+      course.pictureUrl = req.body.pictureUrl
 
+      course.save()
+        .then(() => res.json('Course Updated'))
+        .catch(err => res.status(400).json('Error: ' + err))
+    })
 });
 
 router.route('/delete/:id').delete((req, res) => {
