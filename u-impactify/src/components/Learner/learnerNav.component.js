adff0e085850d220c0b6b485654188b2a7f20c78
TonyXu-FX <71341094+TonyXu-FX@users.noreply.github.com>
2020-12-07 12:38:56 -0500

    TX fix aligment of items on dashboard (#70)

--- u-impactify/src/components/Learner/learnerNav.component.js
+++ u-impactify/src/components/Learner/learnerNav.component.js
@@ -7,26 +7,36 @@ import ProfileBar from '../Profile/profile-navbar.component'
 import { Grid } from '@material-ui/core'
 
 export default class LearnerBar extends Component {
-    render() {
-        return (
-            <Grid container direction = "row" className="my-4">
-                <Grid item>
-                    <ProfileBar/>
-                </Grid>
-                <Grid xs container direction="column">
-                    <Grid item>
-                        <div className='top-navv'>
-                            <h1 className="dashboard">Dashboard</h1>
-                        </div>
-                    </Grid>
-                </Grid>    
-                <Grid>
-                    <h1 className = "calendar">Calendar</h1>
-                    <h1 className = "announcements">Announcements</h1>
-                    <h1 className = "classes">Your Classes</h1>
-                    <h1 className = "courses">Suggested Courses</h1>
-                </Grid>
-            </Grid>  
-        );
-    }
+  render() {
+    return (
+      <Grid container direction="row" className="my-4">
+        <Grid item>
+          <ProfileBar />
+        </Grid>
+        <Grid item xs container direction="column">
+          <Grid item>
+            <div className='top-navv'>
+              <h1 className="dashboard">Dashboard</h1>
+            </div>
+          </Grid>
+          <Grid item container direction="row" style={{ paddingTop: '20px' }}>
+            <Grid item xs={6}>
+              <h1 className="calendar">Calendar</h1>
+            </Grid>
+            <Grid item xs={6}>
+              <h1 className="announcements">Announcements</h1>
+            </Grid>
+          </Grid>
+          <Grid item container direction="row" style={{ paddingTop: '300px' }}>
+            <Grid item xs={6}>
+              <h1 className="classes">Your Classes</h1>
+            </Grid>
+            <Grid item xs={6}>
+              <h1 className="courses">Suggested Courses</h1>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    );
+  }
 }
